cc2dfbcee2447dfa87949bbfbee1aa90cb68c6ff
Alonso Lamas <lamas.alonso@gmail.com>
2024-01-10 15:42:51 +0100

    Add cardForm aestethics updates

--- src/components/CardForm.vue
+++ src/components/CardForm.vue
@@ -10,6 +10,7 @@ import * as geofire from 'geofire-common'
 import { usePermission } from '@vueuse/core'
 import { useFileUpload } from '@/composables/useFileUpload'
 import type { styleKey, cardInfoType } from '@/types'
+import { shallowRef } from 'vue'
 
 const props = defineProps({
   card: {
@@ -23,6 +24,9 @@ const emit = defineEmits(['submit', 'reset'])
 const { coords } = useGeolocation()
 const geolocationAccess = usePermission('geolocation')
 const { handleFileUpload } = useFileUpload()
+
+const tab = shallowRef(null)
+const hasCustomStamp = shallowRef(false)
 
 const cardInfo = ref<cardInfoType>({
   id: '',
@@ -113,76 +117,106 @@ watch(
         demo
       />
     </div>
-    <div class="grid grid-cols-5 gap-4">
-      <div class="col-span-3">
-        <BaseFileInput
-          :imageSource="cardStyle.backgroundImage || ''"
-          inputId="backgroundImage"
-          label="Background image"
-          @change.stop="($event) => handleFileUpload($event, 'backgroundImage', setStyleKeyValue)"
-        />
-        <BaseColorPicker v-model="cardStyle.backgroundColor">Background</BaseColorPicker>
-        <BaseColorPicker v-model="cardStyle.color">text</BaseColorPicker>
-      </div>
-      <div class="flex flex-col gap-2">
-        <BaseColorPicker v-model="cardStyle.borderColor">Border</BaseColorPicker>
-        <VTextField
-          v-model="cardStyle.borderWidth"
-          label="Border width"
-          type="number"
-          suffix="px"
-        />
-      </div>
-      <div class="flex flex-col gap-2">
-        <BaseFileInput
-          :imageSource="cardStyle.stampActiveImage || ''"
-          inputId="stampActiveImage"
-          label="Stamp active image"
-          @change.stop="($event) => handleFileUpload($event, 'stampActiveImage', setStyleKeyValue)"
-        />
-        <BaseColorPicker v-model="cardStyle.stampActiveColor">Active stamp</BaseColorPicker>
-        <BaseColorPicker v-model="cardStyle.stampInactiveColor"> Default stamp </BaseColorPicker>
-        <VTextField v-model="cardInfo.maxCount" label="Max count" type="number" />
-        <v-select
-          v-model="cardInfo.icon"
-          label="Icon"
-          :items="[
-            'mdi-coffee-outline',
-            'mdi-coffee',
-            'mdi-mushroom',
-            'mdi-alien',
-            'mdi-arm-flex',
-            'mdi-bacteria',
-            'mdi-hamburger',
-            'mdi-fruit-pineapple'
-          ]"
-        ></v-select>
-      </div>
-      <div class="col-span-2">
-        <h3 class="text-xl mb-8">Info</h3>
-        <VTextField v-model="cardInfo.name" label="Name" required />
-        <VTextarea v-model="cardInfo.description" label="Description" counter="500" noResize />
-      </div>
-      <div class="col-span-3">
-        <h3 class="text-xl mb-8">Location</h3>
-        <div class="w-full h-96">
-          <BaseMap
-            :center="{ lat: cardLocation.lat, lng: cardLocation.lng }"
-            :markers="[cardLocation]"
-            :markerProps="{ draggable: true }"
-            @handlePinUpdate="handlePinUpdate"
-          />
+    <v-tabs v-model="tab" align-tabs="start">
+      <v-tab :value="1">General</v-tab>
+      <v-tab :value="2">Style</v-tab>
+    </v-tabs>
+    <v-window v-model="tab">
+      <v-window-item :value="1">
+        <div class="flex gap-4 m-4">
+          <div class="flex flex-col basis-1/3">
+            <VTextField v-model="cardInfo.name" label="Name" required />
+            <VTextField v-model="cardInfo.maxCount" label="Max count" type="number" />
+            <VTextarea v-model="cardInfo.description" label="Description" counter="500" noResize />
+            <VBtn
+              v-if="geolocationAccess === 'granted'"
+              @click="getCoords"
+              :loading="(coords.latitude && coords.longitude) === Infinity"
+              :disabled="(coords.latitude && coords.longitude) === Infinity"
+              prepend-icon="mdi-map-marker"
+            >
+              Find my location
+            </VBtn>
+          </div>
+          <div class="flex grow">
+            <BaseMap
+              :center="{ lat: cardLocation.lat, lng: cardLocation.lng }"
+              :markers="[cardLocation]"
+              :markerProps="{ draggable: true }"
+              @handlePinUpdate="handlePinUpdate"
+            />
+          </div>
         </div>
+      </v-window-item>
+      <v-window-item :value="2">
+        <div class="flex gap-4 m-4">
+          <div class="basis-1/2 flex flex-col gap-4">
+            <div class="flex gap-4">
+              <div class="basis-1/2 flex flex-col gap-2">
+                <BaseColorPicker v-model="cardStyle.color">Text color</BaseColorPicker>
+                <BaseColorPicker v-model="cardStyle.borderColor">Border</BaseColorPicker>
+                <BaseColorPicker v-model="cardStyle.backgroundColor"
+                  >Background color</BaseColorPicker
+                >
+              </div>
+              <div class="grow">
+                <BaseFileInput
+                  :imageSource="cardStyle.backgroundImage || ''"
+                  inputId="backgroundImage"
+                  label="Background image"
+                  @change.stop="
+                    ($event) => handleFileUpload($event, 'backgroundImage', setStyleKeyValue)
+                  "
+                />
+              </div>
+            </div>
+            <div>
+              <VTextField
+                v-model="cardStyle.borderWidth"
+                label="Border width"
+                type="number"
+                suffix="px"
+              />
+            </div>
+          </div>
+          <div class="basis-1/2 flex flex-col gap-2">
+            <template v-if="hasCustomStamp">
+              <BaseFileInput
+                :imageSource="cardStyle.stampActiveImage || ''"
+                inputId="stampActiveImage"
+                label="Stamp active image"
+                @change.stop="
+                  ($event) => handleFileUpload($event, 'stampActiveImage', setStyleKeyValue)
+                "
+              />
+            </template>
+            <template v-else>
+              <BaseColorPicker v-model="cardStyle.stampActiveColor">Active stamp</BaseColorPicker>
+              <BaseColorPicker v-model="cardStyle.stampInactiveColor">
+                Default stamp
+              </BaseColorPicker>
 
-        <VBtn
-          v-if="geolocationAccess === 'granted'"
-          @click="getCoords"
-          :loading="(coords.latitude && coords.longitude) === Infinity"
-          :disabled="(coords.latitude && coords.longitude) === Infinity"
-          icon="mdi-map-marker"
-        />
-      </div>
-    </div>
+              <v-select
+                v-model="cardInfo.icon"
+                label="Icon"
+                :items="[
+                  'mdi-coffee-outline',
+                  'mdi-coffee',
+                  'mdi-mushroom',
+                  'mdi-alien',
+                  'mdi-arm-flex',
+                  'mdi-bacteria',
+                  'mdi-hamburger',
+                  'mdi-fruit-pineapple'
+                ]"
+              ></v-select>
+            </template>
+            <v-switch v-model="hasCustomStamp" label="Custom stamp" inset></v-switch>
+          </div>
+        </div>
+      </v-window-item>
+    </v-window>
+
     <div class="flex gap-4 justify-end">
       <VBtn @click="submit" color="primary" :disabled="isDisabled"><slot></slot></VBtn>
       <VBtn :disabled="isDisabled" @click="reset">Reset</VBtn>
